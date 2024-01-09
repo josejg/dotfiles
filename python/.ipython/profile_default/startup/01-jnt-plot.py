43d727ec50ec2c935396c9e0240b7854ca671557
Jose Javier <26491792+josejg@users.noreply.github.com>
2024-01-09 14:43:16 -0800

    Python imports

--- python/.ipython/profile_default/startup/01-jnt-plot.py
+++ python/.ipython/profile_default/startup/01-jnt-plot.py
@@ -1,9 +1,12 @@
 # import jtplot submodule from jupyterthemes
-from jupyterthemes import jtplot
+try:
+    from jupyterthemes import jtplot
 
-# currently installed theme will be used to
-# set plot style if no arguments provided
-jtplot.style()
+    # currently installed theme will be used to
+    # set plot style if no arguments provided
+    jtplot.style()
 
-# from IPython.display import set_matplotlib_formats
-# set_matplotlib_formats('svg', 'pdf')
+    # from IPython.display import set_matplotlib_formats
+    # set_matplotlib_formats('svg', 'pdf')
+except ImportError:
+    pass
--- python/.ipython/profile_default/startup/02-aoc.py
+++ python/.ipython/profile_default/startup/02-aoc.py
@@ -1,6 +1,5 @@
 import re
 import parse
 from collections import Counter, defaultdict
-from aocd.models import Puzzle
 import itertools
 from functools import lru_cache
