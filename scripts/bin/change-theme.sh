9598387242afea9f73d80cf6f8d845006e60726f
Jose Javier <26491792+josejg@users.noreply.github.com>
2024-04-01 16:49:34 -0700

    fix

--- scripts/bin/change-theme.sh
+++ scripts/bin/change-theme.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 PATH="/opt/homebrew/bin/:$PATH"
 if [[ "$1" = "dark" ]]; then
-    cd ~/.config/alacritty
+    cd "$HOME/.config/alacritty" || exit 1
     rm current-theme.toml
     touch alacritty.toml
     # npx alacritty-themes Tomorrow-Night
 elif [[ "$1" = "light" ]]; then
-    cd ~/.config/alacritty
+    cd "$HOME/.config/alacritty" || exit 1
     ln -sf themes/more_themes/Atelierdune.light.toml current-theme.toml
     touch alacritty.toml
     # npx alacritty-themes Atelierdune.light
