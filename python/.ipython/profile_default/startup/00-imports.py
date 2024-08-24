913f7e19153eec25767801372d8e07780754e0d4
Jose Javier <26491792+josejg@users.noreply.github.com>
2024-08-24 11:47:26 -0700

    python imports

--- python/.ipython/profile_default/startup/00-imports.py
+++ python/.ipython/profile_default/startup/00-imports.py
@@ -1,15 +1,15 @@
 import json
 import pathlib
 from pprint import pprint
-import yaml
 
 try:
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import pandas as pd
+    import seaborn as sns
     import torch
+    import yaml
+    from tqdm.autonotebook import tqdm
 except ImportError:
     pass
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from tqdm.autonotebook import tqdm
-import seaborn as sns
